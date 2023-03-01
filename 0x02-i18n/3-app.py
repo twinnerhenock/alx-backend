7d8da461b989279a7665d609480a9a031e65cbae
twinnerhenock <demessiehe@gmail.com>
2023-03-01 13:19:32 +0000

    parametrize templates

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,47 +1,54 @@
 #!/usr/bin/env python3
-"""
-Create a get_locale function with the babel.localeselector
-decorator. Use request.accept_languages to determine the best match
-with our supported languages
-"""
+"""3-Parametrize templates"""
+
+
 from flask import Flask, render_template, request
-from typing import Any
 from flask_babel import Babel
 
-app = Flask(__name__)
-babel = Babel(app)
 
+# Create a Config class
+class Config:
+    """
+        COnfiguration class for Bable
+        It confugures the default language to be English,
+        and the default timezone to be UTC
+        it has a languages class attributes equal to ["en", "fr"]
+    """
 
-class Config(object):
-    """
-    Config class that has a LANGUAGES class attribute
-    equal to ["en", "fr"]
-    """
     LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = 'en'
-    BABEL_DEFAULT_TIMEZONE = 'UTC'
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
-app.config.from_object("3-app.Config")
-
-
-@app.route("/", strict_slashes=False)
-def hello_world() -> Any:
+# create a get_locale function that uses request.accept_languages
+# and returns the best match with our supported languages
+def get_locale():
+    """returns the best match with our supported languages
     """
-    return index.html template that simply outputs
-    “Welcome to Holberton” as page title (<title>) and
-    “Hello world” as header (<h1>)
-    """
-    return render_template("3-index.html")
 
-
-@babel.localeselector
-def get_locale() -> Any:
-    """
-    determine the best match with our supported languages
-    """
     return request.accept_languages.best_match(app.config["LANGUAGES"])
 
 
+# Create an instance of Flask
+app = Flask(__name__)
+# Create an instance of Babel
+babel = Babel(app)
+# Configure the app with the Config class
+app.config.from_object(Config)
+# Initialize Babel with the app and the get_locale function
+babel.init_app(app, locale_selector=get_locale)
+
+
+# Create a route for the index page
+# that renders the index.html template
+@app.route("/", methods=["GET"], strict_slashes=False)
+def index():
+    """renders index.html
+    """
+
+    return render_template("3-index.html")
+
+
+# Run the app only if this file is called directly
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run()
